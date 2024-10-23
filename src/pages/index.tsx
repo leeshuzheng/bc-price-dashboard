d9a9ba497dfd567fa25f0282a3d20b5ffc82102d
Hadi Lee <leeshuzheng@gmail.com>
2024-10-24 06:34:18 +0800

    fix: revert

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { baseApiUrl } from "@/constants";
 import { Page } from "@/templates";
 import { Coin } from "@/types";
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   try {
     const res = await fetch(
       `${baseApiUrl}/simple/price?ids=${process.env.NEXT_PUBLIC_SUPPORTED_CURRENCIES}&vs_currencies=usd&include_24hr_change=true&include_24hr_vol=true&include_market_cap=true`
